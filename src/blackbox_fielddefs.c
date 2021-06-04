8168d7ebc61760e17721df566b68be4624a9c02f
Jonathan Hudson <jh+github@daria.co.uk>
2021-06-04 21:58:31 +0100

    update rc_modes for 3.0 firmware

--- src/blackbox_fielddefs.c
+++ src/blackbox_fielddefs.c
@@ -61,42 +61,52 @@ const char * const FLIGHT_LOG_FLIGHT_MODE_NAME_BETAFLIGHT[] = {
 };
 
 const char * const FLIGHT_LOG_FLIGHT_MODE_NAME_INAV[] = {
-    "ARM",
-    "ANGLE",
-    "HORIZON",
-    "NAVALTHOLD",
-    "HEADINGHOLD",
-    "HEADFREE",
-    "HEADADJ",
-    "CAMSTAB",
-    "NAVRTH",
-    "NAVPOSHOLD",
-    "MANUAL",
-    "BEEPERON",
-    "LEDLOW",
-    "LIGHTS",
-    "NAVLAUNCH",
-    "OSD",
-    "TELEMETRY",
-    "BLACKBOX",
-    "FAILSAFE",
-    "NAVWP",
-    "AIRMODE",
-    "HOMERESET",
-    "GCSNAV",
-    "KILLSWITCH",
-    "SURFACE",
-    "FLAPERON",
-    "TURNASSIST",
-    "AUTOTRIM",
-    "AUTOTUNE",
-    "CAMERA1",
-    "CAMERA2",
-    "CAMERA3",
-    "OSDALT1",
-    "OSDALT2",
-    "OSDALT3",
-    "NAVCRUISE",
+    "ARM",	// 0
+    "ANGLE",	// 1
+    "HORIZON",	// 2
+    "NAVALTHOLD",	// 3
+    "HEADINGHOLD",	// 4
+    "HEADFREE",	// 5
+    "HEADADJ",	// 6
+    "CAMSTAB",	// 7
+    "NAVRTH",	// 8
+    "NAVPOSHOLD",	// 9
+    "MANUAL",	// 10
+    "BEEPERON",	// 11
+    "LEDLOW",	// 12
+    "LIGHTS",	// 13
+    "NAVLAUNCH",	// 14
+    "OSD",	// 15
+    "TELEMETRY",	// 16
+    "BLACKBOX",	// 17
+    "FAILSAFE",	// 18
+    "NAVWP",	// 19
+    "AIRMODE",	// 20
+    "HOMERESET",	// 21
+    "GCSNAV",	// 22
+    "KILLSWITCH",	// 23
+    "SURFACE",	// 24
+    "FLAPERON",	// 25
+    "TURNASSIST",	// 26
+    "AUTOTRIM",	// 27
+    "AUTOTUNE",	// 28
+    "CAMERA1",	// 29
+    "CAMERA2",	// 30
+    "CAMERA3",	// 31
+    "OSDALT1",	// 32
+    "OSDALT2",	// 33
+    "OSDALT3",	// 34
+    "NAVCOURSEHOLD",	// 35
+    "BRAKING",	// 36
+    "USER1",	// 37
+    "USER2",	// 38
+    "FPVANGLEMIX",	// 39
+    "LOITERDIRCHN",	// 40
+    "MSPRCOVERRIDE",	// 41
+    "PREARM",	// 42
+    "TURTLE",	// 43
+    "NAVCRUISE",	// 44
+    "AUTOLEVEL",	// 45
     NULL
 };
 
