448c850b9c4bede02fa05f970e1b4dda4c157ab2
Jonathan Hudson <jh+github@daria.co.uk>
2024-05-23 12:39:20 +0100

    updated rcmode order for 8.0 - cope with removed entries

--- tools/rcmodes.rb
+++ tools/rcmodes.rb
@@ -35,7 +35,9 @@ def process_enums wanted,have,enums
 
     puts "/* #{have} */"
     processed.each_with_index do |l,j|
-      next if l.nil?
+      if l.nil?
+        l = "**REMOVED**"
+      end
       case have
       when 'boxId_e'
         l = l.sub(/^BOX/,'')
